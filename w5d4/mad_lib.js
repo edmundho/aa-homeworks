15d7d3f930f26c580f1ba2b92b4ceea3b74b65ba
Ed <edmundho9@gmail.com>
2018-05-16 22:40:44 -0700

    w5d4 homework initial commit

---
+++ w5d4/mad_lib.js
@@ -0,0 +1,6 @@
+function madLib (string1, string2, string3) {
+  return `We shall ${string1} the ${string2} ${string3}`;
+}
+
+console.log(madLib("shoot", "damn", "basketball"))
+console.log(madLib("cook", "best", "shakshukah"))
