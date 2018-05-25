60f329faa26aef6facb235c1645c67b7b772a1c2
Ed <edmundho9@gmail.com>
2018-05-24 21:50:43 -0700

    Finish npm 'getting started' homework

---
+++ w6d5/npm_hw/webpack.config.js
@@ -0,0 +1,25 @@
+
+
+module.exports = {
+  entry: "./entry.jsx",
+  output: {
+    // path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: './bundle.js'
+  },
+  module: {
+    rules: [{
+      test: [/\.jsx?$/],
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      },
+    }]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
